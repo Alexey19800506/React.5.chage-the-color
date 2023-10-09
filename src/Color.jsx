7babbef783618533aeca6c74e7cd79c1b17f5f02
Alexey19800506 <osiyuk19800506@gmail.com>
2023-10-09 11:59:20 +0300

    t4

---
+++ src/Color.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const RED = '#f00';
+const GREEN = '#0f0';
+const BLUE = '#00f';
+
+class Counter extends React.Component {
+ 
+setBodyColor = color => {
+    document.body.style.backgroundColor = color;
+}
+
+    render() {
+        return (
+          <div className="colors">
+            <button style={{backgroundColor: RED}} className="colors__button" onClick={() => this.setBodyColor(RED)} />
+            <button style={{backgroundColor: GREEN}} className="colors__button" onClick={() => this.setBodyColor(GREEN)} />
+            <button style={{backgroundColor: BLUE}} className="colors__button" onClick={() => this.setBodyColor(BLUE)} />
+          </div>
+        );
+    }
+}
+   
+export default Counter;
+
